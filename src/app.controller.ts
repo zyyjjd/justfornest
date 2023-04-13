48080ed8ecc032f8bf98d422fca99e4ac046ad83
jinjd <2499124163@qq.com>
2023-04-13 15:35:09 +0800

    📃 docs: 小修改

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,14 @@
+/*
+ * @Date: 2023-04-13 15:32:46
+ * @LastEditTime: 2023-04-13 15:33:36
+ * @FilePath: /justfornest/src/app.controller.ts
+ * @Description: 测试nest
+ *
+ */
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('test')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
